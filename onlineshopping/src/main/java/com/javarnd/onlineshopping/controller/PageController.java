c5c73fedada2378bb1b886ce4e2f39d7f46422d1
prasoon <prasoon.javarnd@gmail.com>
2018-05-12 01:12:44 +0530

    Successfully loaded static resources

---
+++ onlineshopping/src/main/java/com/javarnd/onlineshopping/controller/PageController.java
@@ -0,0 +1,43 @@
+package com.javarnd.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = {"/", "/home", "/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to spring mvc");
+		return mv;
+	}
+	
+	
+	
+	
+	
+	//Only for learning purpose
+	
+/*	@RequestMapping(value ="/test")
+	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting) {
+		if(greeting==null) {
+			greeting = "Hello World";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}*/
+	
+/*	@RequestMapping(value ="/test/{greeting}")
+	public ModelAndView test(@PathVariable(value="greeting") String greeting) {
+		if(greeting==null) {
+			greeting = "Hello World";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}*/
+}
